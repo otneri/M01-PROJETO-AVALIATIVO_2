b103b7f551201dec71554d75278266ea29fc00d1
Otavio Neri <otavioneri47@gmail.com>
2022-09-16 11:33:36 -0300

    useeffect no provider

--- src/contexts/Autenticação/AuthPrivider.jsx
+++ src/contexts/Autenticação/AuthPrivider.jsx
@@ -1,27 +1,43 @@
-import { createContext, useState } from 'react';
-import PropTypes from 'prop-types'
+import { createContext, useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
 export const ContextAuth = createContext();
 
-export const AuthProvider = ({children}) => {
-    const [usuario, setUsuario] = useState(null);
+export const AuthProvider = ({ children }) => {
+  const [usuario, setUsuario] = useState({
+    nome: "",
+    senha: "",
+  });
 
-    const handleLogin = ({email, senha}) => {
-        console.log(email, senha);
-        setUsuario({nome: 'Otavio', email: 'otavioneri@gmail'});
+  const handleLogin = ({ email, senha }) => {
+    console.log(email, senha);
+    setUsuario({ nome: "Otavio", email: "otavioneri@gmail" });
+  };
+
+  useEffect(() => {
+    const userStorage = localStorage.getItem("fullName");
+    if (userStorage) {
+      setUsuario(JSON.parse(userStorage));
+    } else {
+      setUsuario({
+        nome: "",
+      });
     }
+  }, []);
 
-    const handleLogout = () => {
-        setUsuario(null);
-    };
+  const handleLogout = () => {
+    setUsuario(null);
+  };
 
-    return (
-        <ContextAuth.Provider value={{isAutenticado: !!usuario , handleLogin, handleLogout}}>
-            {children}
-        </ContextAuth.Provider>
-    )
-}
+  return (
+    <ContextAuth.Provider
+      value={{ isAutenticado: !!usuario, handleLogin, handleLogout }}
+    >
+      {children}
+    </ContextAuth.Provider>
+  );
+};
 
 AuthProvider.propTypes = {
-    children: PropTypes.node
-}
+  children: PropTypes.node,
+};
